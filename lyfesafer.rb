a4c1d21e88b34e51b5ad551289e5f4dc01fdcc76
Peter Schüllermann <peter.schuellermann@gmail.com>
2016-02-14 09:51:19 +0100

    update lyfesafer.rb
    callHomeInhabitant now has code to call the inhabitnats and ask if there is a fire

--- lyfesafer.rb
+++ lyfesafer.rb
@@ -9,8 +9,42 @@
 #     no fire -> abort and log
 #   no or no response -> it calls callFireDepartment()
 
+# requires the following
+# https://api.tropo.com/1.0/sessions?action=create
+# &token=TOKEN
+# &customerTelephone=14075551212
+# &customerName=John+Dyer
+# &msg=the+sky+is+falling
+
+def callHomeInhabitant()
+
+  call '+' + $customerTelephone
+
+  say "Hello " + $customerName + ", we received note that your house may be on fire!"
+
+  log "The home inhabitants are called " + $customerName
+
+  owner_at_home = ask "Are you at home?", {
+    :choices => "yes, no"}
+
+  say "You said " + owner_at_home.value
+
+  log "The inhabitants are at home: " + owner_at_home.value
 
 
-def callHomeInhabitant(){
+end
 
-}
+
+
+
+def callNeighbours()
+
+
+end
+
+
+
+
+def callFiredepartment()
+
+end
